0cfdebf88e9bc6f90f29ef28168c8b33ff5a659a
Diego Vera <omar.vera@gmail.com>
2018-07-30 21:23:36 +0200

    Implements listing of window status

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import Lib
+import Control.Monad (forM_)
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class
+
+listOfWindows = ["git-prompt", "tmux-auto"]
 
 main :: IO ()
-main = someFunc
+main =
+  forM_ listOfWindows getStatus
+  where
+    getStatus windowName = do
+      processName <- runMaybeT $ getRunningProcessOnWindow $ windowName ++ ":1"
+      case processName of
+        Nothing -> putStrLn windowName
+        Just name -> putStrLn $ windowName ++ "    " ++ name
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
 module Lib
-    ( someFunc
+    ( getRunningProcessOnWindow
     ) where
+
+import System.Process
+import Data.Maybe
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class
+import Control.Monad
+import System.Exit
+import Text.Printf
+import Data.List.Split
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+getRunningProcessOnWindow :: String -> MaybeT IO String
+getRunningProcessOnWindow windowNameAndPane = do 
+    ( exitCode, processNumber, _ ) <- lift $ readProcessWithExitCode "tmux" ["list-panes", "-t", windowNameAndPane, "-F", "'#{pane_pid}'"] []
+    guard ( exitCode == ExitSuccess )
+    getChilProcessName $ removeSingleQuotes $ removeEndOfLine processNumber
+
+getChilProcessName :: String -> MaybeT IO String
+getChilProcessName processNumber = do
+    ( exitCode, childProcessNumber, _) <- lift $ readProcessWithExitCode "bash" ["-c", "pgrep -P " ++ processNumber] []
+    guard ( exitCode == ExitSuccess )
+    let childProcessNumberWithoutFormat = removeEndOfLine childProcessNumber
+    ( exitCodeChild, childProcessName, _ ) <- lift $ readProcessWithExitCode "bash" ["-c", "ps -p " ++  childProcessNumberWithoutFormat] []
+    guard ( exitCodeChild == ExitSuccess )
+    return $ ( last . splitOn "/" ) $ removeEndOfLine childProcessName
+
+removeEndOfLine :: String -> String
+removeEndOfLine = filter (/= '\n')
+
+removeSingleQuotes :: String -> String
+removeSingleQuotes = filter (/= '\'')
